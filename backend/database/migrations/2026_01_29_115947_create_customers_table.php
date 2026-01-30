8bf520686892a0a4094a7b0701e494080fbd993b
Jay Anne Marie Tuatis <jayannemariemugartuatis@gmail.com>
2026-01-30 11:09:24 +0800

    feat: resolve port conflicts, setup nginx proxy, and fix database migrations for customers

--- backend/database/migrations/2026_01_29_115947_create_customers_table.php
+++ backend/database/migrations/2026_01_29_115947_create_customers_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('customers', function (Blueprint $table) {
+        $table->id();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('email')->unique();
+        $table->string('contact_number');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
